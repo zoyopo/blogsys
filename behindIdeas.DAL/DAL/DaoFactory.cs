fa0173a7d22d41eb6489251c69d06855b85708a6
godlikedeveloper <694534942@qq.com>
2017-08-01 14:08:04 +0800

    Add files via upload
    data getting layer

---
+++ behindIdeas.DAL/DAL/DaoFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace behindIdeas.DAL.DAL
+{
+    public class DaoFactory<T> where T : class,new()
+    {
+
+        public static T t = null;
+        public static T GetInstance()
+        {
+            t = new T();
+            return t;
+
+        }
+    }
+}
---
+++ behindIdeas.DAL/DAL/IDao.cs
@@ -0,0 +1,40 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace behindIdeas.DAL.DAL
+{
+  public  interface IDao<T> where T : class
+    {
+        void Add(T entity);
+
+        void Add(IList<T> list);
+
+        /// <summary>
+        /// 根据实体
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(T entity);
+        /// <summary>
+        /// 根据属性名
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="enums"></param>
+        void Update(T entity, Enum enums);
+    
+        void Delete(T entity);
+
+        void Delete(IList<T> list);
+        IQueryable<T> Getlist();
+        IQueryable<T> Find_By_Filter(Expression<Func<T, Boolean>> filter);
+        PageResults<T> PaginationDetail(IQueryable<T> paramList, int pageNo, int pageSize);
+        IQueryable<T> Pagination(IQueryable<T> paramList,int pageNo, int pageSize);
+        void transaction();
+        void addNotsave(T entity);
+    }
+}
